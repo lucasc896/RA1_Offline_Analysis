9cf26227d096926836b398aa9bed4fe6027a6c30
Chris Lucas <chris.lucas@bristol.ac.uk>
2013-12-06 14:03:24 +0000

    add in new jet category

--- Run_All_RA1.sh
+++ Run_All_RA1.sh
@@ -2,27 +2,27 @@
 
 
 # Make Tex Tables
-for i in {"2","3","all"}
+for i in {"1","2","3","all"}
 do
    ./Prediction_RA1.py -n $i
 done
 
 
 # Make Uncorrected Tex Tables
-for i in {"2","3","all"}
+for i in {"1","2","3","all"}
 do
    ./Prediction_RA1.py -u $i
 done
 
 # Make Root Files
-for i in {"2","3"}
+for i in {"1","2","3"}
 do
    ./Prediction_RA1.py -r $i
 done
 
 
 # Make Closure Tests
-for i in {"2","3","all","jetcat"}
+for i in {"1","2","3","all","jetcat"}
 do
    ./Prediction_RA1.py -c $i
 done
