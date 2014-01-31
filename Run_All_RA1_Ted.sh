2ddb557517bc2f4eb122d7bb6c04c3dee5ef05ac
Chris Lucas <chris.lucas@bristol.ac.uk>
2014-01-31 10:27:03 +0000

    add run script for Ted's specific output

---
+++ Run_All_RA1_Ted.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+# Make Tex Tables
+for i in {"2","3","all"}
+do
+   ./Prediction_RA1.py -n $i
+done
+
+
+# # Make Uncorrected Tex Tables
+# for i in {"2","3","all"}
+# do
+#    ./Prediction_RA1.py -u $i
+# done
+
+# Make Root Files
+#for i in {"2","3"}
+#do
+#   ./Prediction_RA1.py -r $i
+#done
+
