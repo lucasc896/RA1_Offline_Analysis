afb4bb1914684924876ceb6184df769044d70d55
Chris Lucas <chris.lucas@bristol.ac.uk>
2014-11-04 10:07:06 +0000

    remove u tables

--- Run_All_RA1.sh
+++ Run_All_RA1.sh
@@ -8,11 +8,11 @@ do
 done
 
 
-# Make Uncorrected Tex Tables
-for i in {"2","3","all"}
-do
-   ./Prediction_RA1.py -u $i
-done
+## Make Uncorrected Tex Tables
+#for i in {"2","3","all"}
+#do
+#   ./Prediction_RA1.py -u $i
+#done
 
 # Make Root Files
 for i in {"2","3"}
